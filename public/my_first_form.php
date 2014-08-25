dd72c91390067041b1773578dce8a45d7b326152
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2014-08-25 11:25:48 -0500

    Edited to add labels

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -11,9 +11,11 @@
 <body>
 	<form method="POST">
 		<p>
+			<label for="first_name">First Name</label>
 			<input type="text" name="first_name" id="first_name"/>
 		</p>
 		<p>
+			<label for="last_name">Last Name</label>
 			<input type="text" name="last_name" id="last_name"/>
 		</p>
 		<input type="submit"/>
